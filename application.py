d7c52cffb213454bf43f93d1992a3f0a063a29ab
JeremyTica <jeremytica@gmail.com>
2023-11-10 08:57:15 -0500

    Comments and Refinement 1.0
    Added docstrings, comments giving context and explaining the purpose notable definitions, class, and variables.

--- application.py
+++ application.py
@@ -5,6 +5,10 @@ import random
 import os 
 
 class Product:
+    """_summary_
+    Create the product into an object and define its distinct features:
+    The product code, name, sale price, manufacture cost, stock level, and the monthly units that will be manufactured
+    """
     def __init__(self, code, name, sale_price, manufacture_cost, stock_level, estimated_monthly_units):
         self.code = code
         self.name = name
@@ -14,6 +18,9 @@ class Product:
         self.estimated_monthly_units = estimated_monthly_units
 
     def show(self):
+        """_summary_
+        Display information about the product
+        """
         print("Product Code:", self.code)
         print("Product Name:", self.name)
         print("Product Cost: $"+str(self.sale_price)+ " CAD")
@@ -22,20 +29,27 @@ class Product:
         print("Estimate monthly production:", self.estimated_monthly_units)
 
     def product_sales(self):
-        unfulfilled_sales = False
+        """_summary_
+        Manages the simulations of product sales, and projects the units sold, produced, and a total net-profit
+        """
+        unfulfilled_sales = False #! Create local variable unfulfilled sales which will be used as a condition to dictate whether or not sales are unfulfilled during the simulated model
 
         #! Initial Stock
         print("\nMonth 1")
-        number_of_units_sold = 10 * random.randint(1,10)
+        print("Manufactured:", self.estimated_monthly_units)
+        number_of_units_sold = 10 * random.randint(1,10) #! Simulation of units sold; different each month
         print(number_of_units_sold, "Units Sold")
-        stock_difference = self.stock_level + self.estimated_monthly_units - number_of_units_sold
+        stock_difference = self.stock_level + self.estimated_monthly_units - number_of_units_sold #! Since this is the first month, the stock level is put into account along with the number of units produced within the month
         print("Current Stock:", stock_difference)
 
-        total_units_sold = number_of_units_sold
-        total_units_manufactured = self.estimated_monthly_units
+        total_units_sold = number_of_units_sold #! Re-occurring counter which will continue to store a totalled value of units sold
+        total_units_manufactured = self.estimated_monthly_units #! Re-occurring counter which will continue to store a totalled value of estimated units produced monthly
 
+        #! Simulated model of sales over the span of 12 months
         for months in range(1, 12):
             print("\nMonth", months+1)
+
+            print("Manufactured:", self.estimated_monthly_units)
 
             stock_difference += self.estimated_monthly_units #! Units being produced monthly 
             total_units_manufactured += self.estimated_monthly_units
@@ -45,7 +59,7 @@ class Product:
                 print("Out of Stock!")
                 break
             else:
-                number_of_units_sold = 10 * random.randint(1,10) 
+                number_of_units_sold = 10 * random.randint(1,10)  
 
                 stock_difference -= number_of_units_sold
                 if stock_difference <= 0: #! Under the condition that the stock goes under 0, it will ask the user whether or not they wish to sell the remaining stock from the previous month, and the stock produced from the current month
@@ -75,6 +89,7 @@ class Product:
                     print(number_of_units_sold, "Units Sold")
                     print("Current Stock:", stock_difference)
 
+        #! Report/Statistics
         if unfulfilled_sales:
             unfulfilled_sales_value = unfulfilled_sales_value * -1
             print(unfulfilled_sales_value, "sales could not be fulfilled.")
@@ -82,10 +97,12 @@ class Product:
         print("\n\nTotal Units Sold:", total_units_sold)
         print("Total Stock Produced: ", total_units_manufactured)
         
-        net_profit = total_units_sold * self.sale_price - total_units_manufactured * self.manufacture_cost
-        print("Net-profit: $", format(net_profit,".2f"))
+        net_profit = (total_units_sold * self.sale_price) - (total_units_manufactured * self.manufacture_cost)
+        print("Net-profit: $", format(net_profit,".2f")) #! Display only 2 decimal places
 
 def ask_product_code():
+    os.system("cls")
+    print("Product Inventory Simulation and Analysis")
     while True:
         global p_code
         p_code = int(input("\nPlease input the product code: "))
